7d21aa9a90293740bc22f6448ef29f75fb439711
Pablo <pablolm11@gmail.com>
2023-09-02 16:42:22 +0200

    imports

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,4 +1,6 @@
+import { Inter } from 'next/font/google'
 import '../globals.css'
+import { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {

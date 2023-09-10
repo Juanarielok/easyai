bcc7695c61128d00df1e1d29840f780cf132982a
Pablo <pablolm11@gmail.com>
2023-09-10 23:05:48 +0200

    ia

--- services/conexion.ts
+++ services/conexion.ts
@@ -1,19 +1,25 @@
 import Mensaje from "@/models/mensaje";
+import RespuestaSobreTexto from "@/models/respuesta-sobre-texto";
 
-  function verIArespuesta (){
+import { pipeline } from '@xenova/transformers';
 
-    let mensajetexto : Mensaje = { esRespuestaIA : true ,
+function verIArespuesta() {
 
-        mensaje : "pepito" ,
-    
-    } 
+  let mensajetexto: Mensaje = {
+    esRespuestaIA: true,
+    mensaje: "pepito"
+  };
 
-return mensajetexto;
+  return mensajetexto;
+}
 
-  }
+export async function responderPregunta( contexto : string, pregunta : string) : Promise<RespuestaSobreTexto> {
+  let ia = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
+  let output = await ia(pregunta, contexto);
+
+  return output;
+}
 
 
- 
 
-
-  export default verIArespuesta;
+export default verIArespuesta;

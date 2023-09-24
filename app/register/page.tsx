e3d2676d997d8737e4730dc11dbf0e7aca3095e6
Jose <josebalserospinto@gmail.com>
2023-09-24 14:39:18 -0400

    cambios simples

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,22 +2,28 @@
 
 import './page.scss';
 import './registerEstilo.scss';
+import Link from 'next/link';
 import React, {useState} from 'react';
 
 function Registro () {
 
     return(
         <div className="principal">
-            <div className= "cajaTexto">
+
+            <div className= "contenedor-titulo">
                 <h1>Don´t have an account at AI Easy?</h1>
                 <h2>Sign up to gain access to all our features</h2>
             </div>
-            <div className='labelTexto'>
-            <input type="email" name="email" placeholder='Your email'className='texto'></input>
+
+            <div className='contenedor-label'>
+                <input type="text" name="email" placeholder='Your email'className='texto' id="emailTexto"/>
             </div>
-            <div className= "contenedorBoton">
-                <button className='botonEnviar'>Submit</button>
+
+            <div className= "contenedor-boton">
+                <button className='botonEnviar' id='enviar'>Submit</button>
+                <h3>Already have an account? Sign in <Link href={'./register/Login'} className='enlace-login'>here</Link></h3>
             </div>
+
         </div>
     )
 }

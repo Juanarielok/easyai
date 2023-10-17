b5e2dc6f77636fbbc99d6e56102128afac7af35e
Jose <josebalserospinto@gmail.com>
2023-10-17 17:53:39 -0400

    Changes to the Menu Bar using useRef hook

--- components/menuDesplegable/MenuDesplegable.tsx
+++ components/menuDesplegable/MenuDesplegable.tsx
@@ -1,24 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Menu from '../menu/menu';
 import './menu-estilo.scss';
 
 export default function MenuDesplegable () {
     const [menuBoton, mostrarMenu] = useState(true);
+    const toggleMenu = useRef<HTMLDivElement>(null);
 
     function manejarClick () {
         mostrarMenu(!menuBoton);
     }
+
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+          if (toggleMenu.current && !toggleMenu.current.contains(event.target)) {
+            mostrarMenu(true);  
+          }
+        };
+      
+        document.addEventListener('mousedown', handleClickOutside);
+      
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, []);
+      
 
     return(
         <div 
             className='estilo-menu-desplegable'
             onClick={manejarClick} 
             style={{
-                transition: "width 1.5s linear",
+                transition: menuBoton ? "none" : "width 0.4s linear",
                 width: menuBoton ? "0%" : "10%",
                 position: menuBoton ? "inherit" : "fixed",
-                right: menuBoton ? "inherit" : "0px"
+                right: menuBoton ? "inherit" : "0px",
+                marginRight: menuBoton ? "60px" : "5px",
+                marginTop: menuBoton ? "15px" : "0px"
               }}
+            ref= {toggleMenu}
         >
             {menuBoton && <img src='/icons/menu-desplegable.png' className='menu-icono' alt='Menu Icono'></img>}
 

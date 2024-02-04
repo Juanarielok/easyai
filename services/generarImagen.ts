003988525e55af83ba98ab7233690b21262d9936
Jose <josebalserospinto@gmail.com>
2024-02-04 14:11:41 -0500

    Selección de múltiples modelos para la generación de imágenes es ahora posible

--- services/generarImagen.ts
+++ services/generarImagen.ts
@@ -1,12 +1,16 @@
-import axios from "axios";
+import axios, { Axios, AxiosResponse } from "axios";
 import mensajeChat from "../models/mensaje";
 
-export default async function generarImagen(input: string) {
-  let mensaje: mensajeChat = {
+export default async function generarImagen(
+  input: string,
+  modelo: string
+): Promise<AxiosResponse> {
+  let body = {
     mensaje: input,
+    modelo: modelo,
   };
 
-  const respuesta = await axios.post("http://localhost:3002/imagen", mensaje);
+  const respuesta = await axios.post("http://localhost:3002/imagen", body);
 
   return respuesta;
 }
